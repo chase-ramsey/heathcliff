87607ea9e9c61b0513ba05cb173bb718c400c232
Zak Spence <zakspence@gmail.com>
2017-06-17 18:00:08 -0500

    my post request is refused yayyyyyy

--- client.py
+++ client.py
@@ -5,22 +5,38 @@ import sys
 # We are going to connect to the server
 # Then we take a message we've written and post it.
 
-def main(ip):
+def main(ip, custom_port=None):
     
+    # check that we have an ip address
     if len(ip) == 0:
         sys.exit("You need to give an ip address or a domain name to connect to")
 
+    # deal with ports that aren't default 80
+    if custom_port:
+        port = str(custom_port)
+    else:
+        port = '80'
+
+    # create our full connection address
+    address = 'http://{}:{}'.format(ip, port)
+    print(address)
+
+    # main loop
     on = True
     while on:
         
-        message = input('>').encode('utf-8')
-        if message == 'quit':
+        message = input('> ').encode('utf-8')
+        if message.decode('utf-8') == 'quit':
             sys.exit()
-        r = requests.post(ip, data=message)
-
+        r = requests.post(address, data=message)
+        print(r.status_code)
 
 if __name__ == '__main__':
 
-    ip = sys.argv[x]
-    main(ip)
+    try:
+        ip = sys.argv[1]
+        port = sys.argv[2]
+        main(ip, port)
+    except IndexError:
+        main(ip)
 
